ca7d8f9fc39d4759e670f10b53f2687ade69aa0c
TarheK <42652896+TarheK@users.noreply.github.com>
2018-11-27 10:41:08 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def render_home():
     return render_template('home.html')
+@app.route("/")
+def render_page1():
+    return render_template('page1.html')
   
 if __name__=="__main__":
     app.run(debug=False, port=54321)
