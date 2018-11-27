1dac97ce752194b663315578e38b432bf94842ff
TarheK <42652896+TarheK@users.noreply.github.com>
2018-11-27 11:00:59 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,17 @@ def render_home():
     return render_template('home.html')
 @app.route("/page1")
 def render_page1():
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
     return render_template('page1.html')
-  
+  def getDataOptions(counties):
+    myList=[] 
+    for county in counties:
+        if not county["State"] in myList:
+            myList.append (county["State"])
+    options = ""
+    for state in myList:
+        options=options + Markup("<option value=\"" + state + "\">" + state + "</option>")
+    return options
 if __name__=="__main__":
     app.run(debug=False, port=54321)
