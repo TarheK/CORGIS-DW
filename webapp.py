ee2d327a99aa22958ca05b4876a18b9b87b08969
TarheK <42652896+TarheK@users.noreply.github.com>
2018-11-28 08:18:22 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,11 +3,18 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+    @app.route("/")
 def render_home():
     return render_template('home.html')
-@app.route("/page1")
+    @app.route("/page1")
 def render_page1():
-    return render_template('page1.html')
+    @app.route("/response2"
+def render_response1():
+    age group = 26+
+    response=hi
+    return render_template('response.html', response = response)
+   
 if __name__=="__main__":
     app.run(debug=False, port=54321)
+
+  
